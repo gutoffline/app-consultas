fd889cc4ae4d15cc3138c420e2692236d5a5da40
Guto Xavier <gutoffline@gmail.com>
2021-05-11 20:08:37 -0300

    Criação da página index e da estrutura de pastas padrão

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>APP Consultas</title>
+</head>
+<body>
+    <main>
+        <header>
+            <h1>APP Consultas</h1>
+            <hr>
+        </header>
+        <nav>
+            <a href="#"><img src="img/medico.png">Cadastro de médicos</a>
+            <a href="#"><img src="img/paciente.png">Cadastro de pacientes</a>
+            <a href="#"><img src="img/agenda.png">Agendamento de consultas</a>
+        </nav>
+        <div class="conteudo">
+            <h2>Conteúdo da aplicação</h2>
+        </div>
+        <footer>
+            &copy; Todos os direitos reservados
+        </footer>
+    </main>
+</body>
+</html>
