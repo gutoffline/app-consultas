dc9f8181671593254709cfe1efcefee6168c1c99
Guto Xavier <gutoffline@gmail.com>
2021-05-11 21:44:45 -0300

    inclusão do cabeçalho e do rodapé

--- index.php
+++ index.php
@@ -1,31 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>APP Consultas</title>
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-</head>
-<body>
-    <main class="container">
-        <header>
-            <h1>APP Consultas</h1>
-            <hr>
-        </header>
-        <nav>
-            <a href="#"><img src="img/medico.png">Cadastro de médicos</a>
-            <a href="#"><img src="img/paciente.png">Cadastro de pacientes</a>
-            <a href="#"><img src="img/agenda.png">Agendamento de consultas</a>
-        </nav>
+<?php include "cabecalho.php"; ?>
+        
         <div class="conteudo">
             <h2>Conteúdo da aplicação</h2>
         </div>
-        <footer>
-            &copy; Todos os direitos reservados
-        </footer>
-    </main>
-    <script src="bootstrap/js/bootstrap.bundle.js"></script>
-    <script src="js/jquery.js"></script>
-</body>
-</html>
+
+<?php include "rodape.php"; ?>
