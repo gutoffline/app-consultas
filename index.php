10c69241062d72cd1dcd9c67c5344fff9cb26739
Guto Xavier <gutoffline@gmail.com>
2021-05-13 20:07:27 -0300

    Listagem de médicos

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php include "cabecalho.php"; ?>
-        
-        <div class="conteudo">
-            <h2>Conteúdo da aplicação</h2>
-        </div>
-
+    <div class="conteudo">
+        <h2>Conteúdo da aplicação</h2>
+    </div>
 <?php include "rodape.php"; ?>
